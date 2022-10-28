b2e32dfca161c99f208a245df09fd7388a13efe9
Alireza Doustmohammadi <a.doustmohammadi@modares.ac.ir>
2022-10-28 22:13:16 +0330

    files have been removed

--- VinaDock/afronp_p1 +/vina_screen_local.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-for f in ligand_*.pdbqt; do
-    b=`basename $f .pdbqt`
-    mkdir -p $b
-    ./../bin/vina --config conf.txt --ligand $f --out ${b}/out.pdbqt --log ${b}/log.txt
-done
